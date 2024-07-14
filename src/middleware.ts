8584ad5b210d4c5dc7e201343de9c81767be4ab4
Jorge Matos <jisapjisap@gmail.com>
2024-07-14 16:40:53 +0200

    Clerk configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/settings(.*)"])  // Ruta protegida, necesita de logeo
+const isProtectedRoute = createRouteMatcher(["/settings(.*)","/"])  // Rutas protegidas, necesita de logeo
 
 export default clerkMiddleware((auth, req) => {   // auth proporciona métodos para manejar la autenticación, req es la solicitud de comprobación de la ruta
   if(isProtectedRoute(req)) auth().protect()      // Si la ruta es protegida ("/settings") -> necesitas de login para poder visualizarla
