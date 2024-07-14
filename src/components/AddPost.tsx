fdf75d148635b83b7c309420b37fd2d7dfedf75e
Jorge Matos <jisapjisap@gmail.com>
2024-07-14 18:30:45 +0200

    addPost

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -53,7 +53,18 @@ const AddPost = () => {
 
         {/* POST OPTIONS */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
-          uploadwidget
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
         </div>
 
       </div>
