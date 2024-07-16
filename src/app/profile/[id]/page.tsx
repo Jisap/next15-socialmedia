77405d5669150a8b6fb70610b271013aabebb628
Jorge Matos <jisapjisap@gmail.com>
2024-07-16 16:27:58 +0200

    profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -5,17 +5,17 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div className="flex gap-6 pt-6">
+    <div className='flex gap-6 pt-6'>
       <div className="hidden xl:block w-[20%]">
         <LeftMenu />
       </div>
-      <div className='w-full lg:w-[70%] xl:w-[50%]'>
-        <div className='flex flex-col gap-6'>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
           <Feed />
         </div>
-        <div className='hidden lg:block w-[30%]'>
-          <RightMenu />
-        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
       </div>
     </div>
   )
