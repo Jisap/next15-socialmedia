f7f9463dda108263181b7a15ec84e26ca9f89e26
Jorge Matos <jisapjisap@gmail.com>
2024-07-16 20:26:03 +0200

    userMediaCard

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -10,6 +11,66 @@ const UserMediaCard = ({ userId }: { userId: string }) => {
           <Link href="/" className="text-blue-500 text-xs">
             See all
           </Link>
+      </div>
+
+      {/* BOTTOM */}
+      <div className='flex gap-4 justify-between flex-wrap'>
+        <div className='relative w-1/5 h-24'>
+          <Image 
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' 
+            alt='' 
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
+        <div className='relative w-1/5 h-24'>
+          <Image
+            src='https://images.pexels.com/photos/17470785/pexels-photo-17470785/free-photo-of-view-of-foamy-waves-breaking-on-a-rocky-shore.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=''
+            fill className='object-cover rounded-md'
+          />
+        </div>
       </div>
     </div>
   )
