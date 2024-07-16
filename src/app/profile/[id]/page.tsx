2e4825623c30e2e3ae5682bb0441354fd0aa36fb
Jorge Matos <jisapjisap@gmail.com>
2024-07-16 21:13:41 +0200

    profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Feed from '@/components/feed/Feed'
 import LeftMenu from '@/components/leftmenu/LeftMenu'
 import RightMenu from '@/components/rightMenu/RightMenu'
+import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = () => {
@@ -11,6 +12,41 @@ const ProfilePage = () => {
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-full h-64 relative">
+              <Image
+                src={"/noCover.png"}
+                alt=""
+                fill
+                className="rounded-md object-cover"
+              />
+              <Image
+                src={"/noAvatar.png"}
+                alt=""
+                width={128}
+                height={128}
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+              />
+            </div>
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              Edward Grariel May
+            </h1>
+
+            <div className="flex items-center justify-center gap-12 mb-4">
+              <div className="flex flex-col items-center">
+                <span className="font-medium">500</span>
+                <span className="text-sm">Posts</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">1200</span>
+                <span className="text-sm">Followers</span>
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium">120</span>
+                <span className="text-sm">Following</span>
+              </div>
+            </div>
+          </div>
           <Feed />
         </div>
       </div>
