47fdf7ee580b943b3fab77a1171573210b5ce521
Jorge Matos <jisapjisap@gmail.com>
2024-07-19 16:29:53 +0200

    profile

--- src/components/leftmenu/ProfileCard.tsx
+++ src/components/leftmenu/ProfileCard.tsx
@@ -1,19 +1,42 @@
+import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfileCard = () => {
+const ProfileCard = async () => {
+
+  const { userId } = auth();
+
+  if(!userId) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId
+    },
+    include:{
+      _count: {
+        select:{
+          followers: true
+        }
+      }
+    }
+  })
+
+  if (!user) return null;
+  console.log(user);
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
       <div className="h-20 relative">
         <Image
-          src="/noCover.png"
+          src={user.cover || "/noCover.png"}
           alt=""
           fill
           className="rounded-md object-cover"
         />
         <Image
-          src="/noAvatar.png"
+          src={user.avatar || "/noAvatar.png"}
           alt=""
           width={48}
           height={48}
@@ -22,7 +45,11 @@ const ProfileCard = () => {
       </div>
       <div className="h-20 flex flex-col gap-2 items-center">
         <span className="font-semibold">
-          Edward Grariel May
+          {
+            user.name && user.surname
+              ? user.name + " " + user.surname
+              : user.username
+          }
         </span>
         <div className="flex items-center gap-4">
           <div className="flex">
@@ -49,10 +76,10 @@ const ProfileCard = () => {
             />
           </div>
           <span className="text-xs text-gray-500">
-            500 Followers
+            {user._count.followers} Followers
           </span>
         </div>
-        <Link href={`/profile`}>
+        <Link href={`/profile/${user.username}`}>
           <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
             My Profile
           </button>
